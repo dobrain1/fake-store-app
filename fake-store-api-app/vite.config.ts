7096716dcf093c3f9343e32269568445f996530f
dobrain1 <dobrovolsky1993@gmail.com>
2025-02-20 17:46:54 +0300

    gh-pages

--- fake-store-api-app/vite.config.ts
+++ fake-store-api-app/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/fake-store-app/',
   plugins: [react()],
   resolve: {
     alias: {
